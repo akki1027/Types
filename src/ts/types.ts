bbdd7b963b6d96117344003b201a212497d9ac4d
akki1027 <akki10271995@gmail.com>
2021-10-31 17:07:20 +0900

    型宣言とunderscoreのインストール

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,29 +1,11 @@
 'use strict';
 
+import * as _ from 'underscore';
+
+
 {
-    // オリジナルの型の定義
-    type MyBool = true | false;
-    type WindowStates = "open" | "closed" | "minimized";
-    type LockStates = "locked" | "unlocked";
-    type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
-
-    const hasMoney: MyBool = true;
-    console.log(hasMoney);
-
-    const root: HTMLElement | null = document.getElementById("root");
-
-    // interface
-    interface User {
-        id: number;
-        name: string;
-    };
-
-    const user: User = {
-        id: 1,
-        name: 'yamada'
-    };
-
-    console.log(user);
+    /************************************************************************/
+    // 関数
 
     const printId = (id: number | string) => {
         if (typeof id === "string") {
@@ -34,6 +16,110 @@
             console.log(id);
         }
     };
+    // printId(1);
+    // printId('id');
 
-    printId(1);
+
+    /************************************************************************/
+    //interface
+
+    interface User {
+        id: number;
+        name: string;
+    };
+
+    const user: User = {
+        id: 1,
+        name: 'yamada'
+    };
+    // console.log(user);
+
+
+    /************************************************************************/
+    // class
+
+    class UserAccount {
+        id: number;
+        name: string;
+
+        constructor(id: number, name: string) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    const userTwo: User = new UserAccount(2, "aki");
+    // console.log(userTwo);
+
+
+    /************************************************************************/
+    // オリジナルの型の定義
+
+    type MyBool = true | false;
+    type WindowStates = "open" | "closed" | "minimized";
+    type LockStates = "locked" | "unlocked";
+    type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+    const hasMoney: MyBool = true;
+    // console.log(hasMoney);
+
+    /************************************************************************/
+    // Generics
+
+    type StringArray = Array<string>;
+    type NumberArray = Array<number>;
+    type ObjectWithNameArray = Array<{ name: string }>;
+
+
+    /************************************************************************/
+    //DOM 操作
+
+    // const root: HTMLElement = document.getElementById('root')!;
+    // or
+    const root: HTMLElement = <HTMLInputElement>document.getElementById('root')!;
+    root.style.color = 'red';
+
+    const a = document.createElement('a');
+    a.href = "#";
+    a.textContent = 'ボタンです';
+    root.append(a);
+
+
+    /************************************************************************/
+    // practice
+
+    const printName = (obj: { first: string; last?: string }): void => {
+        console.log(obj);
+    }
+    printName({ first: "Bob" });
+    printName({ first: "Alice", last: "Alisson" });
+
+    const shaffled: number[] = _.shuffle([1, 2, 3, 4, 5]);
+    console.log(shaffled);
+
+    interface Test {
+        name: string;
+        score: number;
+    };
+
+    const test: Test = {
+        name: 'math',
+        score: 100,
+    };
+
+
+    // keys キーを全て取得
+    console.log(_.keys(test));
+
+
+    // values バリューを全て取得
+    console.log(_.values(test));
+
+
+    // invert キーとバリューを入れ替える
+    console.log(_.invert(test));
+
+
+    // keyが含まれているかどうかを真偽値で返す
+    console.log(_.has(test, 'name'));
 }
